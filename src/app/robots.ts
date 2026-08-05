964a221b1c99525ea353d7ccdc1ca667663cd9b8
febinthomas540-source <febin.thomas540@gmail.com>
2026-08-05 02:59:52 +0100

    Add search crawler directives

---
+++ src/app/robots.ts
@@ -0,0 +1,17 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = "https://intezaar.vercel.app";
+
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/_next/"],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
+  };
+}
