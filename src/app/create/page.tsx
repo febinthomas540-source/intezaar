db9af40273ba991ff5b6f66a9bf3a0b27e283b94
febinthomas540-source <febin.thomas540@gmail.com>
2026-08-06 19:53:09 +0100

    Activate mobile polish and prototype letter transfer

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,15 +1,18 @@
 "use client";
 
+import { CreatorShareBridge } from "@/components/creator-share-bridge";
 import { LetterCreator } from "@/components/letter-creator";
 import { ShortLetterUnlock } from "@/components/short-letter-unlock";
 import "../photo-adjustment.css";
 import "../seal-post.css";
+import "../mobile-creator-polish.css";
 
 export default function CreatePage() {
   return (
     <>
       <LetterCreator />
       <ShortLetterUnlock />
+      <CreatorShareBridge />
     </>
   );
 }
