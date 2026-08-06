769c1fdfef4a5e30b6e505fa8c9c8ac12e84699d
febinthomas540-source <febin.thomas540@gmail.com>
2026-08-06 19:21:09 +0100

    Enable short letters on the create page

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import { LetterCreator } from "@/components/letter-creator";
+import { ShortLetterUnlock } from "@/components/short-letter-unlock";
 import "../photo-adjustment.css";
 import "../seal-post.css";
 
 export default function CreatePage() {
-  return <LetterCreator />;
+  return (
+    <>
+      <LetterCreator />
+      <ShortLetterUnlock />
+    </>
+  );
 }
