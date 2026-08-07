9570aabe6ba9f33bf0d1ddce39d96ab053d2e643
febinthomas540-source <febin.thomas540@gmail.com>
2026-08-07 12:54:04 +0100

    Remove creator DOM bridges

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { CreatorMediaBridge } from "@/components/creator-media-bridge";
-import { CreatorShareBridge } from "@/components/creator-share-bridge";
-import { MediaUploadLimitGuard } from "@/components/media-upload-limit-guard";
 import { StableLetterCreator } from "@/components/stable-letter-creator";
 import { TurnstilePostingGuard } from "@/components/turnstile-posting-guard";
 import "../photo-adjustment.css";
@@ -16,9 +13,6 @@ export default function CreatePage() {
   return (
     <>
       <StableLetterCreator />
-      <MediaUploadLimitGuard />
-      <CreatorMediaBridge />
-      <CreatorShareBridge />
       <TurnstilePostingGuard />
     </>
   );
